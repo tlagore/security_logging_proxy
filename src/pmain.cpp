5b22693d72bbb70e34a0d2f5871e65b9273e04c8
tyrone.lagore <tyrone.lagore@zone46-ec.cs.ucalgary.ca>
2017-02-06 17:11:23 -0700

    parsing arguments for main

--- src/pmain.cpp
+++ src/pmain.cpp
@@ -10,10 +10,13 @@
 using namespace std;
 
 #include "proxy_master.h"
+#include "proxy_worker.h"
 
 int main(int argc, char *argv[])
 {
-  int portNum;
+  int srcPort, dstPort;
+  int n = -1, logOption;
+  char *server, *autoN;
 
   /* TODO handle extra args, consider using optarg */
   if (argc < 4){
@@ -24,8 +27,32 @@ int main(int argc, char *argv[])
       //arg format [options -raw -strip -hex -autoN] listenPort server serverPort
       if(argc == 4){
 	//no option log specified
+	cout << "No log option specified, defaulting to raw output." << endl;
+	srcPort = stoi(argv[1]);
+	server = argv[2];
+	dstPort = stoi(argv[3]);
       }else{
-	//log option specified
+	if(strcmp(argv[1], "-raw") == 0){
+	  logOption = RAW;
+	  printf("raw\n");
+	}else if(strcmp(argv[1], "-strip") == 0){
+	  logOption = STRIP;
+	  printf("strip\n");
+	}else if(strcmp(argv[1], "-hex") == 0){
+	  logOption = HEX;
+	  printf("hex\n");
+	}else if(strncmp(argv[1], "-auto", 5) == 0){
+	  logOption = AUTO_N;
+	  //+ 1 for null terminator
+	  autoN = (char*)calloc((int)strlen(argv[1]) - 4, 0);
+	  strncpy(autoN, argv[1] + 5, strlen(argv[1]) - 5);
+	  autoN[(int)strlen(argv[1]) - 5] = '\0';
+
+	  n = stoi(autoN);
+	  printf("auto %d\n",autoN, n);
+	}else{
+	  printf("Invalid log option, defaulting to raw\n");
+	}
       }
       // portNum = stoi(argv[1]);
       // server.setPort(portNum);
